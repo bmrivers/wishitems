34c9c2550d7e172e6268fc90283f984991924ac3
Bonny R <bonnyrivers@gmail.com>
2019-03-22 12:25:47 -0700

    working app

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+# only one model
+
+class Item(models.Model):
+    description = models.TextField(max_length=100)
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Add the following import
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+import requests
+from .models import Item
+
+# Define the home view
+def home(request):
+    items_list = Item.objects.all()
+    return render(request, 'index.html', {'items_list': items_list})
+
+class ItemCreate(CreateView):
+    model = Item
+    fields = '__all__'
+    success_url = '/'
