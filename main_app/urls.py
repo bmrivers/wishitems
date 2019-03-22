427574c7a64adc52a4e8d25f63979ba7d6e7ab99
Bonny R <bonnyrivers@gmail.com>
2019-03-22 12:34:36 -0700

    working delete button

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('new/', views.ItemCreate.as_view(), name="item_create")
+    path('new/', views.ItemCreate.as_view(), name="item_create"),
+    path('delete/<int:item_id>', views.delete_item, name="item_delete")
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -12,6 +12,12 @@ def home(request):
     items_list = Item.objects.all()
     return render(request, 'index.html', {'items_list': items_list})
 
+def delete_item(request, item_id):
+    item = Item.objects.get(id=item_id)
+    item.delete()
+    success_url = '/'
+    return home(request)
+
 class ItemCreate(CreateView):
     model = Item
     fields = '__all__'
